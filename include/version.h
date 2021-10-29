999afebcb68ba640cac07838a82e8ec7217d5653
ermiry <erick.salas@ermiry.com>
2021-10-28 23:50:08 -0500

    Version 0.1

--- include/version.h
+++ include/version.h
@@ -4,7 +4,7 @@
 #define SERVICE_VERSION				"0.1"
 #define SERVICE_VERSION_NAME		"Version 0.1"
 #define SERVICE_VERSION_DATE		"28/10/2021"
-#define SERVICE_VERSION_TIME		"23:46 CST"
+#define SERVICE_VERSION_TIME		"23:50 CST"
 #define SERVICE_VERSION_AUTHOR		"Erick Salas"
 
 // print full service version information
